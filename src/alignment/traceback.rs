1ad0468bc96ac8c0d2e8766fd38b079716bfa6ec
Jack Roddy <jack.w.roddy@gmail.com>
2022-12-21 14:07:44 -0700

    implemented Trace, it's probably buggy
    The Trace struct is used to store the results of tracing back through an
    "optimal accuracy" DP matrix. I think this is going to be a target for
    future refactoring.

--- src/alignment/traceback.rs
+++ src/alignment/traceback.rs
@@ -1,3 +1,269 @@
-use crate::structs::{DpMatrix, Profile};
+use crate::structs::profile::constants::{
+    PROFILE_BEGIN_TO_MATCH, PROFILE_DELETE_TO_DELETE, PROFILE_DELETE_TO_MATCH,
+    PROFILE_INSERT_TO_INSERT, PROFILE_INSERT_TO_MATCH, PROFILE_MATCH_TO_DELETE,
+    PROFILE_MATCH_TO_INSERT, PROFILE_MATCH_TO_MATCH, SPECIAL_B, SPECIAL_C, SPECIAL_E, SPECIAL_J,
+    SPECIAL_LOOP, SPECIAL_MOVE, SPECIAL_N,
+};
+use crate::structs::trace::constants::{
+    TRACE_B, TRACE_C, TRACE_D, TRACE_E, TRACE_I, TRACE_J, TRACE_M, TRACE_N, TRACE_S, TRACE_T,
+};
+use crate::structs::{DpMatrix, Profile, Trace};
 
-pub fn traceback(profile: &Profile, posterior_matrix: &DpMatrix, optimal_matrix: &DpMatrix) {}
+pub fn traceback(
+    profile: &Profile,
+    posterior_matrix: &DpMatrix,
+    optimal_matrix: &DpMatrix,
+    trace: &mut Trace,
+) {
+    let mut i = optimal_matrix.target_length;
+    let mut k = 0;
+
+    let mut posterior_probability: f32;
+    let mut previous_state: usize = TRACE_C;
+    let mut current_state: usize;
+
+    trace.append_with_posterior_probability(TRACE_T, k, i, 0.0);
+    trace.append_with_posterior_probability(TRACE_C, k, i, 0.0);
+
+    while previous_state != TRACE_S {
+        match previous_state {
+            TRACE_M => {
+                current_state = select_m(profile, optimal_matrix, i, k);
+                k -= 1;
+                i -= 1;
+            }
+            TRACE_D => {
+                current_state = select_d(profile, optimal_matrix, i, k);
+                k -= 1;
+            }
+            TRACE_I => {
+                current_state = select_i(profile, optimal_matrix, i, k);
+                i -= 1;
+            }
+            TRACE_N => {
+                current_state = select_n(i);
+            }
+            TRACE_C => {
+                current_state = select_c(profile, posterior_matrix, optimal_matrix, i);
+            }
+            TRACE_J => {
+                current_state = select_j(profile, posterior_matrix, optimal_matrix, i);
+            }
+            TRACE_E => {
+                current_state = select_e(profile, optimal_matrix, i, &mut k);
+            }
+            TRACE_B => {
+                current_state = select_b(profile, optimal_matrix, i);
+            }
+            _ => {
+                panic!("bogus state in traceback")
+            }
+        }
+
+        // if current_state == -1 {
+        //     panic!("traceback choice failed")
+        // }
+
+        posterior_probability =
+            get_posterior_probability(posterior_matrix, current_state, previous_state, k, i);
+        trace.append_with_posterior_probability(current_state, k, i, posterior_probability);
+
+        if (current_state == TRACE_N || current_state == TRACE_J || current_state == TRACE_C)
+            && current_state == previous_state
+        {
+            i -= 1;
+        }
+        previous_state = current_state;
+    }
+}
+#[inline(always)]
+pub fn get_posterior_probability(
+    optimal_matrix: &DpMatrix,
+    current_state: usize,
+    previous_state: usize,
+    target_idx: usize,
+    profile_idx: usize,
+) -> f32 {
+    return match current_state {
+        TRACE_M => optimal_matrix.get_match(target_idx, profile_idx),
+        TRACE_I => optimal_matrix.get_insert(target_idx, profile_idx),
+        TRACE_N => {
+            return if current_state == previous_state {
+                optimal_matrix.get_special(target_idx, SPECIAL_N)
+            } else {
+                0.0
+            }
+        }
+        TRACE_C => {
+            return if current_state == previous_state {
+                optimal_matrix.get_special(target_idx, SPECIAL_C)
+            } else {
+                0.0
+            }
+        }
+        TRACE_J => {
+            return if current_state == previous_state {
+                optimal_matrix.get_special(target_idx, SPECIAL_J)
+            } else {
+                0.0
+            }
+        }
+        _ => 0.0,
+    };
+}
+
+#[inline(always)]
+pub fn select_m(
+    profile: &Profile,
+    optimal_matrix: &DpMatrix,
+    target_idx: usize,
+    profile_idx: usize,
+) -> usize {
+    let possible_states: [usize; 4] = [TRACE_M, TRACE_I, TRACE_D, TRACE_B];
+    let possible_paths: [f32; 4] = [
+        profile.transition_score_delta(PROFILE_MATCH_TO_MATCH, profile_idx - 1)
+            * optimal_matrix.get_match(target_idx - 1, profile_idx - 1),
+        profile.transition_score_delta(PROFILE_INSERT_TO_MATCH, profile_idx - 1)
+            * optimal_matrix.get_insert(target_idx - 1, profile_idx - 1),
+        profile.transition_score_delta(PROFILE_DELETE_TO_MATCH, profile_idx - 1)
+            * optimal_matrix.get_delete(target_idx - 1, profile_idx - 1),
+        profile.transition_score_delta(PROFILE_BEGIN_TO_MATCH, profile_idx - 1)
+            * optimal_matrix.get_special(target_idx - 1, SPECIAL_B),
+    ];
+
+    let mut argmax: usize = 0;
+    for i in 1..4 {
+        if possible_paths[i] > possible_paths[argmax] {
+            argmax = i;
+        }
+    }
+    possible_states[argmax]
+}
+
+#[inline(always)]
+pub fn select_d(
+    profile: &Profile,
+    optimal_matrix: &DpMatrix,
+    target_idx: usize,
+    profile_idx: usize,
+) -> usize {
+    let match_to_delete_path = profile
+        .transition_score_delta(PROFILE_MATCH_TO_DELETE, profile_idx - 1)
+        * optimal_matrix.get_match(target_idx, profile_idx - 1);
+    let delete_to_delete_path = profile
+        .transition_score_delta(PROFILE_DELETE_TO_DELETE, profile_idx - 1)
+        * optimal_matrix.get_delete(target_idx, profile_idx - 1);
+
+    return if match_to_delete_path >= delete_to_delete_path {
+        TRACE_M
+    } else {
+        TRACE_D
+    };
+}
+
+#[inline(always)]
+pub fn select_i(
+    profile: &Profile,
+    optimal_matrix: &DpMatrix,
+    target_idx: usize,
+    profile_idx: usize,
+) -> usize {
+    let match_to_insert_path = profile.transition_score_delta(PROFILE_MATCH_TO_INSERT, profile_idx)
+        * optimal_matrix.get_match(target_idx - 1, profile_idx);
+    let insert_to_insert_path: f32 = profile
+        .transition_score_delta(PROFILE_INSERT_TO_INSERT, profile_idx)
+        * optimal_matrix.get_insert(target_idx - 1, profile_idx);
+
+    return if match_to_insert_path >= insert_to_insert_path {
+        TRACE_M
+    } else {
+        TRACE_I
+    };
+}
+
+#[inline(always)]
+pub fn select_n(target_idx: usize) -> usize {
+    return if target_idx == 0 { TRACE_S } else { TRACE_N };
+}
+
+#[inline(always)]
+pub fn select_c(
+    profile: &Profile,
+    posterior_matrix: &DpMatrix,
+    optimal_matrix: &DpMatrix,
+    target_idx: usize,
+) -> usize {
+    let c_to_c_path = profile.special_transition_score_delta(SPECIAL_C, SPECIAL_LOOP)
+        * (optimal_matrix.get_match(target_idx - 1, SPECIAL_C)
+            + posterior_matrix.get_special(target_idx, SPECIAL_C));
+    let c_to_e_path = profile.transition_score_delta(SPECIAL_E, SPECIAL_MOVE)
+        * optimal_matrix.get_special(target_idx, SPECIAL_E);
+
+    return if c_to_c_path >= c_to_e_path {
+        TRACE_C
+    } else {
+        TRACE_E
+    };
+}
+
+#[inline(always)]
+pub fn select_j(
+    profile: &Profile,
+    posterior_matrix: &DpMatrix,
+    optimal_matrix: &DpMatrix,
+    target_idx: usize,
+) -> usize {
+    let j_to_j_path = profile.special_transition_score_delta(SPECIAL_J, SPECIAL_LOOP)
+        * (optimal_matrix.get_match(target_idx - 1, SPECIAL_J)
+            + posterior_matrix.get_special(target_idx, SPECIAL_J));
+    let e_to_j_path = profile.transition_score_delta(SPECIAL_E, SPECIAL_LOOP)
+        * optimal_matrix.get_special(target_idx, SPECIAL_E);
+
+    return if j_to_j_path >= e_to_j_path {
+        TRACE_J
+    } else {
+        TRACE_E
+    };
+}
+
+#[inline(always)]
+pub fn select_e(
+    profile: &Profile,
+    optimal_matrix: &DpMatrix,
+    target_idx: usize,
+    return_profile_idx: &mut usize,
+) -> usize {
+    let mut max = -f32::INFINITY;
+    let mut state_max = 0;
+    let mut profile_idx_max = 0;
+
+    for profile_idx in 1..=profile.length {
+        if optimal_matrix.get_match(target_idx, profile_idx) >= max {
+            max = optimal_matrix.get_match(target_idx, profile_idx);
+            state_max = TRACE_M;
+            profile_idx_max = profile_idx;
+        }
+        if optimal_matrix.get_delete(target_idx, profile_idx) >= max {
+            max = optimal_matrix.get_delete(target_idx, profile_idx);
+            state_max = TRACE_D;
+            profile_idx_max = profile_idx;
+        }
+    }
+
+    *return_profile_idx = profile_idx_max;
+    state_max
+}
+
+#[inline(always)]
+pub fn select_b(profile: &Profile, optimal_matrix: &DpMatrix, target_idx: usize) -> usize {
+    let n_to_b_path = profile.special_transition_score_delta(SPECIAL_N, SPECIAL_MOVE)
+        * optimal_matrix.get_match(target_idx, SPECIAL_N);
+    let j_to_b_path = profile.transition_score_delta(SPECIAL_J, SPECIAL_MOVE)
+        * optimal_matrix.get_special(target_idx, SPECIAL_J);
+
+    return if n_to_b_path >= j_to_b_path {
+        TRACE_N
+    } else {
+        TRACE_J
+    };
+}
--- src/structs/trace.rs
+++ src/structs/trace.rs
@@ -1,4 +1,4 @@
-mod constants {
+pub mod constants {
     pub const TRACE_BOGUS: usize = 0;
     pub const TRACE_M: usize = 1;
     pub const TRACE_D: usize = 2;
@@ -19,10 +19,72 @@ pub struct Trace {
     length: usize,
     profile_length: usize,
     sequence_length: usize,
-    states: Vec<u8>,
+    states: Vec<usize>,
+    /// node index
     k: Vec<usize>,
+    /// position emitted in seq
     i: Vec<usize>,
     posterior_probabilities: Vec<f32>,
 }
 
-impl Trace {}
+impl Trace {
+    pub fn new(profile_length: usize, sequence_length: usize) -> Self {
+        Trace {
+            length: 0,
+            profile_length,
+            sequence_length,
+            states: vec![],
+            k: vec![],
+            i: vec![],
+            posterior_probabilities: vec![],
+        }
+    }
+
+    pub fn append_with_posterior_probability(
+        &mut self,
+        state: usize,
+        k: usize,
+        i: usize,
+        posterior_probability: f32,
+    ) {
+        match state {
+            // TRACE_N => {}
+            // TRACE_C => {}
+            TRACE_J => {
+                if self.states[self.length - 1] == state {
+                    self.i[self.length] = i;
+                    self.posterior_probabilities[self.length] = posterior_probability;
+                } else {
+                    self.i[self.length] = 0;
+                    self.posterior_probabilities[self.length] = 0.0;
+                }
+                self.k[self.length] = 0;
+            }
+            // TRACE_X => {}
+            // TRACE_S => {}
+            // TRACE_B => {}
+            // TRACE_E => {}
+            TRACE_T => {
+                self.i[self.length] = 0;
+                self.posterior_probabilities[self.length] = 0.0;
+                self.k[self.length] = 0;
+            }
+            TRACE_D => {
+                self.i[self.length] = 0;
+                self.posterior_probabilities[self.length] = 0.0;
+                self.k[self.length] = k;
+            }
+            // TRACE_M => {}
+            TRACE_I => {
+                self.i[self.length] = i;
+                self.posterior_probabilities[self.length] = posterior_probability;
+                self.k[self.length] = k;
+            }
+            _ => {
+                panic!("no such state");
+            }
+        }
+        self.states[self.length] = state;
+        self.length += 1;
+    }
+}
