6260b0ab4fd2738d1498763402a8b168a5f1824f
Jack Roddy <jack.w.roddy@gmail.com>
2023-03-29 17:51:20 -0600

    Working on joining non-overlapping clouds
    I've got a solution that almost works, but it skips every other
    anti-diagonal during filling.
    
    I also added a few doc comments.

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,16 @@
 use anyhow::Result;
 use clap::ArgAction;
 use clap::Parser;
-use nale::align::bounded::structs::CloudSearchParams;
+use nale::align::bounded::structs::{CloudBoundGroup, CloudSearchParams};
 use nale::output::output_tabular::write_tabular_output;
 use nale::pipelines::{
     pipeline_bounded, pipeline_naive, BoundedPipelineParams, NaivePipelineParams,
 };
 use nale::structs::hmm::parse_hmms_from_p7hmm_file;
 use nale::structs::{Profile, Sequence};
+use nale::viz::SodaJson;
 use std::io;
+use std::io::stdout;
 use std::path::PathBuf;
 
 #[derive(Parser, Debug)]
@@ -73,17 +75,39 @@ impl Args {
 }
 
 fn main() -> Result<()> {
-    let args = Args::parse();
-    let hmms = parse_hmms_from_p7hmm_file(&args.query)?;
-    let mut profiles: Vec<Profile> = hmms.iter().map(|hmm| Profile::new(hmm)).collect();
-    let targets = Sequence::amino_from_fasta(&args.target)?;
+    let mut b1 = CloudBoundGroup::new(100, 100);
+    let mut b2 = CloudBoundGroup::new(100, 100);
 
-    let params_naive = args.params_naive();
-    let alignments_naive = pipeline_naive(&mut profiles, &targets, &params_naive)?;
+    b1.set(0, 0, 0, 0, 0);
+    b1.set(1, 1, 0, 0, 1);
+    b1.set(2, 2, 0, 0, 2);
+    b1.set(3, 3, 0, 0, 3);
+    b1.set(4, 4, 0, 0, 4);
 
-    let params_bounded = args.params_bounded();
-    let alignments_bounded = pipeline_bounded(&mut profiles, &targets, &params_bounded)?;
+    b2.set(200, 100, 100, 100, 100);
+    b2.set(199, 100, 99, 99, 100);
+    b2.set(198, 100, 98, 98, 100);
+    b2.set(197, 100, 97, 97, 100);
+    b2.set(196, 100, 96, 96, 100);
 
-    write_tabular_output(&alignments_bounded, &mut io::stdout())?;
+    b1.soda_json(&mut stdout())?;
+    println!();
+    b2.soda_json(&mut stdout())?;
+    println!();
+    CloudBoundGroup::join_bounds(&mut b1, &b2)?;
+    println!();
+    b1.soda_json(&mut stdout())?;
+    // let args = Args::parse();
+    // let hmms = parse_hmms_from_p7hmm_file(&args.query)?;
+    // let mut profiles: Vec<Profile> = hmms.iter().map(|hmm| Profile::new(hmm)).collect();
+    // let targets = Sequence::amino_from_fasta(&args.target)?;
+    //
+    // let params_naive = args.params_naive();
+    // let alignments_naive = pipeline_naive(&mut profiles, &targets, &params_naive)?;
+    //
+    // let params_bounded = args.params_bounded();
+    // let alignments_bounded = pipeline_bounded(&mut profiles, &targets, &params_bounded)?;
+    //
+    // write_tabular_output(&alignments_bounded, &mut io::stdout())?;
     Ok(())
 }
