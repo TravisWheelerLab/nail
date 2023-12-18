d5de672b3e688ecc9601812dd85dbfc6fe9798d6
jack <jack.w.roddy@gmail.com>
2023-12-18 05:46:59 -0700

    starting to restructure the project

--- README.md
+++ README.md
@@ -1,22 +1,6 @@
 # nale
-Nale ALigns Efficiently
-
-## About
-
-Nale is a library that performs profile Hidden Markov Model (PHMM) biological sequence alignment.
-
-This libary is in early stages of development. The API should be considered highly unstable, and documentation is lacking.
-
-## Installation
-
-To build nale from source, you'll first need to install Rust and Cargo. 
-The easiest way to do that is to use [rustup](https://rustup.rs/).
-
-Once that's done, you can then build nale:
-
-    git clone https://github.com/TravisWheelerLab/nale
-    cd nale/
-    cargo build
+## DEPRECATED
+We have decided to restructure this project in a way that includes renaming this crate.
 
 ## License
 
--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,30 @@
+#[deprecated(
+    since = "0.1.2",
+    note = "This crate is deprecated. See the readme for more information."
+)]
 pub mod align;
+#[deprecated(
+    since = "0.1.2",
+    note = "This crate is deprecated. See the readme for more information."
+)]
 pub mod alphabet;
+#[deprecated(
+    since = "0.1.2",
+    note = "This crate is deprecated. See the readme for more information."
+)]
 pub mod output;
+#[deprecated(
+    since = "0.1.2",
+    note = "This crate is deprecated. See the readme for more information."
+)]
 pub mod structs;
+#[deprecated(
+    since = "0.1.2",
+    note = "This crate is deprecated. See the readme for more information."
+)]
 mod util;
+#[deprecated(
+    since = "0.1.2",
+    note = "This crate is deprecated. See the readme for more information."
+)]
 pub mod viz;
